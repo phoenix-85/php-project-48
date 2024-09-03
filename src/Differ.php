ad864194f237fe4842b1dec360fa5fe413767b91
phoenix <phoenix-85@github.com>
2024-09-03 20:04:11 +0500

    fix test10

--- src/Differ.php
+++ src/Differ.php
@@ -36,7 +36,7 @@ function makeDiffData(object $data1, object $data2): array
         if (!property_exists($data1, $key)) {
             return [
                 "status" => "added",
-                // @phpstan-ignore-line
+                // @phpstan-ignore-next-line
                 "value" => $makeNode($data2->$key)];
         }
         if (!property_exists($data2, $key)) {
