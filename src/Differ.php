e9dcb9146ba0dbbae64632066cf1906a3ef9cb13
phoenix <phoenix-85@github.com>
2024-09-03 17:50:50 +0500

    fix test3

--- src/Differ.php
+++ src/Differ.php
@@ -6,14 +6,14 @@ use function Differ\Formatter\formatOutput;
 use function Differ\Parsers\getDataFromFile;
 use function Functional\sort as func_sort;
 
-function genDiff($pathToFile1, $pathToFile2, $format = 'stylish'): string
+function genDiff(string $pathToFile1, string $pathToFile2, ?string $format = 'stylish'): string
 {
     $data1 = getDataFromFile($pathToFile1);
     $data2 = getDataFromFile($pathToFile2);
     $diff = makeDiffData($data1, $data2);
     return formatOutput($diff, $format);
 }
-function makeDiffData($data1, $data2): array
+function makeDiffData(object $data1, object $data2): array
 {
     $keys = func_sort(
         array_unique(array_keys(array_merge(get_object_vars($data1), get_object_vars($data2)))),
@@ -25,12 +25,11 @@ function makeDiffData($data1, $data2): array
         if (!is_object($data)) {
             return $data;
         }
+
         $keys = array_keys(get_object_vars($data));
-        $result = [];
-        foreach ($keys as $key) {
-            $result[$key] = ["value" => $makeNode($data->$key)];
-        }
-        return $result;
+        $values = array_map(fn($key) => ["value" => $makeNode($data->$key)], $keys);
+
+        return array_combine($keys, $values);
     };
 
     $makeDiffNode = function ($key, $data1, $data2) use (&$makeDiffNode, $makeNode): array|string {
